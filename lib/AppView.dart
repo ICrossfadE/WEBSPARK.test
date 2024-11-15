74b7be9cde64a2f330502aba922c8eca92de3fc7
Maks <Crossfade143@gmail.com>
2024-11-15 10:46:35 +0200

    first commit

---
+++ lib/AppView.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:webspark_test/pages/HomePage.dart';
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:webspark_test/AppView.dart';
+
+void main() {
+  runApp(const AppView());
+}
